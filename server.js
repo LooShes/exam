54aff26580249ce76d9a8cd27811e64cba64c9d9
lusy.volkov <lusy.volkov@gmail.com>
2020-09-05 15:46:12 +0300

    changes

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const path = require('path')
 const urllib = require('urllib')
-//const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
-//app.use(bodyParser.json())
 
 const port = 8080
 app.listen(port, function () {
